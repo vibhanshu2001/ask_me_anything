dbcfcb8903e84e5313007d059793dc8495f8d7fb
Vibhanshu Garg <vibhanshugarg@Vibhanshus-MacBook-Air.local>
2020-11-02 14:47:16 +0530

    Ask me anything

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,45 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: askpage(),
+      ),
+    ),
+  );
+}
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+class askpage extends StatefulWidget {
+  @override
+  _askpageState createState() => _askpageState();
+}
+
+class _askpageState extends State<askpage> {
+  int ballAnswer = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                setState(() {
+                  ballAnswer = Random().nextInt(5)+1;
+                });
+              },
+                child: Image.asset('images/ball$ballAnswer.png'),
+            ),
+
+          ),
+        ],
       ),
     );
+  }
+}
